4f5484150fbcd412396b2d8c651f92067be39c97
Sudhanshukuma <97438302+Sudhanshukuma@users.noreply.github.com>
2022-05-19 11:42:09 +0530

    l=int(input()) n=int(input()) while(n):     n-=1     w,h=map(int,input().split())     if(w<l or h<l):         print("UPLOAD ANOTHER")     elif(w==h):         print("ACCEPTED")     else:         print("CROP IT")

---
+++ Roy_and_Profile_Picture.py
@@ -0,0 +1,11 @@
+l=int(input())
+n=int(input())
+while(n):
+    n-=1
+    w,h=map(int,input().split())
+    if(w<l or h<l):
+        print("UPLOAD ANOTHER")
+    elif(w==h):
+        print("ACCEPTED")
+    else:
+        print("CROP IT")
