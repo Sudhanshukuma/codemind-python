e8071c7c11b6fe01c2aaa081a117e75b145b6637
Sudhanshukuma <97438302+Sudhanshukuma@users.noreply.github.com>
2022-05-19 11:14:30 +0530

    n=int(input()) for i in range(n):     for j in range(n):         print(chr(65+i),end=' ')     print()

--- print_the_following_pattern.py
+++ print_the_following_pattern.py
@@ -1,4 +1,5 @@
 n=int(input())
-for i in range(1,n+1):
-    print(' '*(n-i),end='')
-    print(10**(2*i-1)//9*i)
+for i in range(n):
+    for j in range(n):
+        print(chr(65+i),end=' ')
+    print()
