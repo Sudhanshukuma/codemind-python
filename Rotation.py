41e1a36f348fe68265ec1939c964e76d82ce6982
Sudhanshukuma <97438302+Sudhanshukuma@users.noreply.github.com>
2022-07-03 23:25:33 +0530

    n=int(input()) while(n):     n-=1     a,b=map(int,input().split())     l=list(map(int,input().split()))[:a]     while(b):         b-=1         t=l[a-1]         del(l[a-1])         f=l         f.insert(0,t)         l=f     print(*l)

---
+++ Rotation.py
@@ -0,0 +1,13 @@
+n=int(input())
+while(n):
+    n-=1
+    a,b=map(int,input().split())
+    l=list(map(int,input().split()))[:a]
+    while(b):
+        b-=1
+        t=l[a-1]
+        del(l[a-1])
+        f=l
+        f.insert(0,t)
+        l=f
+    print(*l) 
