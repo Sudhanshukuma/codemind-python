465eb9bec335ad0a04e0bd70ae96d06bb6c27e4a
Sudhanshukuma <97438302+Sudhanshukuma@users.noreply.github.com>
2022-07-03 23:18:39 +0530

    n=int(input()) t=[] l=list(map(int,input().split()))[:n] m=int(input()) b=list(map(int,input().split()))[:n] for i in range(n):     t.insert(b[i],l[i])     # print(t[i],end=' ') for i in range(n):     print(t[i],end=' ')

---
+++ Create_Target_Array_in_the_Given_Order.py
@@ -0,0 +1,10 @@
+n=int(input())
+t=[]
+l=list(map(int,input().split()))[:n]
+m=int(input())
+b=list(map(int,input().split()))[:n]
+for i in range(n):
+    t.insert(b[i],l[i])
+    # print(t[i],end=' ')
+for i in range(n):
+    print(t[i],end=' ')
