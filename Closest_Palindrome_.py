42229eae09029f873111e51461bbf9919bbce4eb
Sudhanshukuma <97438302+Sudhanshukuma@users.noreply.github.com>
2022-07-11 20:52:20 +0530

    def pa(n):     n=str(n)     if(n==n[::-1]):         return True     else:         return False n=int(input()) a=n+1 b=n-1 while(1):     if pa(a)==1:         break     a+=1 while(1):     if pa(b)==1:         break     b-=1 if abs(a-n)==abs(b-n):     print(b,a) elif abs(a-n)>abs(b-n):     print(b) else:     print(a)

--- Closest_Palindrome_.py
+++ Closest_Palindrome_.py
@@ -1,33 +1,23 @@
-def ty(n):
-    t=n
-    d=0
-    while(t):
-        i=t%10
-        d=d*10+i
-        t=t//10
-    if(d==n):
+def pa(n):
+    n=str(n)
+    if(n==n[::-1]):
         return True
     else:
         return False
 n=int(input())
-i=n-1
+a=n+1
+b=n-1
 while(1):
-    if(ty(i)==1):
+    if pa(a)==1:
         break
-    i-=1
-t=n+1
+    a+=1
 while(1):
-    if(ty(t)==1):
+    if pa(b)==1:
         break
-    t+=1
-if(abs(t-n)>abs(i-n)):
-    print(i)
-elif(abs(t-n)==abs(i-n)):
-    if(n>t):
-        print(t,end=" ")
-        print(i,end=" ")
-    else:
-        print(i,end=" ")
-        print(t,end=" ")
+    b-=1
+if abs(a-n)==abs(b-n):
+    print(b,a)
+elif abs(a-n)>abs(b-n):
+    print(b)
 else:
-    print(t)
+    print(a)
