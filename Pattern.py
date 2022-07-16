4394cd0d8f8d173dbc1f03bec658c80a2d9bee70
Sudhanshukuma <97438302+Sudhanshukuma@users.noreply.github.com>
2022-07-16 10:46:57 +0530

    n=int(input()) t=n-1 for i in range(1,n+1):     print(" "*t,end="")     t-=1     for j in range(1,i+1):         print(j,end="")     for k in range(i-1,0,-1):         print(k,end='')     print()

--- Pattern.py
+++ Pattern.py
@@ -1,5 +1,10 @@
 n=int(input())
+t=n-1
 for i in range(1,n+1):
-    for j in range(1,n+1):
-        print(i,end=' ')
+    print(" "*t,end="")
+    t-=1
+    for j in range(1,i+1):
+        print(j,end="")
+    for k in range(i-1,0,-1):
+        print(k,end='')
     print()
