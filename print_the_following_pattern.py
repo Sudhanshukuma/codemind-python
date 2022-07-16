00be796c1225443a197704fb2f6909ef3a09ebc9
Sudhanshukuma <97438302+Sudhanshukuma@users.noreply.github.com>
2022-07-16 10:40:40 +0530

    n=int(input()) t=n-1 for i in range(1,n+1):     print(" "*t,end="")     t-=1     for j in range(1,n+1):         if i==1 or j==1 or i==n or j==n:             print("*",end="")         else:             print(" ",end="")     print()

--- print_the_following_pattern.py
+++ print_the_following_pattern.py
@@ -1,10 +1,11 @@
 n=int(input())
-j=n-1
+t=n-1
 for i in range(1,n+1):
-    print(" "*j,end="")
-    j-=1
-    for k in range(i-1,0,-1):
-        print(k,end="")
-    for p in range(0,i,1):
-        print(p,end='')
+    print(" "*t,end="")
+    t-=1
+    for j in range(1,n+1):
+        if i==1 or j==1 or i==n or j==n:
+            print("*",end="")
+        else:
+            print(" ",end="")
     print()
